b09eaaf55c076bad4d60bc68484782ee12d9acfd
Musab Mondal <musab.mondal321@gmail.com>
2026-04-06 01:38:41 -0400

    Made an initial Homepage

--- frontend/scemas/lib/firebase.ts
+++ frontend/scemas/lib/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,3 +12,5 @@ const firebaseConfig = {
 };
 
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+export const firestore = getFirestore(app);
